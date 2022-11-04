61d8b33fddeee3ddfb63583a3f99becf39551dc7
myesffi :) <105597445+anbeatrizduarte@users.noreply.github.com>
2022-11-04 15:13:37 -0300

    Update q1.js

--- q1.js
+++ q1.js
@@ -1,9 +1,12 @@
 //1. Escreva um programa que dado o valor de uma conta de restaurante, imprima o valor total a ser pago, considerando que o restaurante cobra 10% de taxa para o gar ̧com.
 
+//pacote para ler os dados do teclado
 const prompt = require('prompt-sync')();
 
+//imprimir na tela (o paseInt serve para dizer que o que vai ser imprimido é texto, string)
 let gasto = parseInt(prompt('Digite o valor gasto: '));
 
+//o símbolo / é para dividir
 taxa = gasto / 10;
 
 console.log(`Valor da conta: ${gasto}`);
